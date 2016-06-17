2fd1406852cc7def111a5a300a006bb82eb9fe17
Arjun Khoosal <arjunkhoosal@gmail.com>
2016-06-17 23:54:03 +0200

    initial commit :)

---
+++ README.md
@@ -0,0 +1,95 @@
+### Express API serving that just makes sense
+
+You think of an API like a directory structure, so it's time you served it out of one.
+
+Define your api like this:
+
+    |-apiDir
+      |-user
+         |-fetch.js
+         |-delete.js
+         |-create.js
+      |-deep
+         |-deeper
+            |-isCervixReached.js
+      |-mcHammer
+         |-touch.js
+
+Each endpoint must export a function which accepts requests and produces json-friendly output.
+**isCervixReached.js**
+
+    module.exports = function isCervixReached(req, callback) {
+      return {
+        bumped: req.body.distanceCovered > 11
+      };
+    }
+
+For async functions, either works:
+ 
+ - return a promise
+ - call the `callback` param supplied to your endpoint
+ 
+Here's how to actually serve the api:
+
+    var express = require('express');
+    var app = express();
+    
+    // you'll want to be able to parse json and querystrings
+    var bodyParser = require('body-parser'); 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
+    
+    // serve away
+    var serveApi = require('expressive-api');
+    app.use(serveApi('./apiDir'));
+    
+    app.listen(3000, function () {
+      console.log('And it's running - easy hey?');
+    });
+
+Now you can hit your api:
+    
+    GET /user/fetch
+    POST /deep/deeper/sodeep/cervix
+
+## Advanced stuff
+
+# Controlling which API files are included
+
+Under the hood, this uses [require directory](https://www.npmjs.com/package/require-directory), so you can use pass in any of its options like this:
+
+    app.use(serveApi('./apiDir'), {
+      whitelist: /\.js$/
+    });
+
+# Define endpoints with whatever params you want
+
+You can specify a `transformParams(req, callback)` function which gets applied to the usual parameters.
+The result should be **an array** of the parameters you want your API endpoints to receive.
+
+Here's one I use:
+
+    function transformParams(req, doneCb) {
+      // combine body and query into the params for the api
+      var params = Object.assign({}, req.body);
+      params = Object.assign(params, req.query);
+      return [params, req.user, callback];
+    }
+    
+    app.use(serveApi('./apiDir'), {
+      transformParams: transformParams
+    });
+    
+# Restricting endpoints to certain HTTP methods
+
+Just add an array of `allowedMethods` as a property of your endpoint function, like this:
+
+    function saveData(req, callback){
+      // do the saving
+    }
+    
+    saveData.allowedMethods = ['POST']
+    
+    module.exports = saveData;
+
+That's all folks!
---
+++ index.js
@@ -0,0 +1,121 @@
+var PrettyError = require('pretty-error');
+var pe = new PrettyError();
+module.exports = function (api, inputOptions) {
+
+  // so we can safely check specific options without everything falling apart
+
+  var defaultOptions = {
+    transformParams: function(req, callback){
+      return [req, callback]; // the identity transform
+    }
+  }
+
+  // we'll be well behaved citizens; we won't change anything
+  var options = Object.assign({}, defaultOptions);
+  options = Object.assign(options, inputOptions);
+
+  var requireDirectoryOptions = Object.assign({}, options);
+  delete requireDirectoryOptions.transformParams;
+
+
+  // if a location is passed for an api, create the object
+  if (typeof api === 'string') {
+    api = require('require-directory')(module.parent, api, requireDirectoryOptions);
+  }
+
+  // now we should have an object represnting the api tree
+  if (typeof api === 'object') {
+
+    console.log('api is', api);
+    return function(req, res, next) {
+      var pathPieces = req.url.split('?')[0].split('/').slice(1);
+
+      var currentAction = api;
+
+      // drill down into api object to find the endpoint function, based on the path pieces
+      for (var i in pathPieces) {
+        if (currentAction.hasOwnProperty(pathPieces[i])) {
+          currentAction = currentAction[pathPieces[i]];
+        } else {
+          break;
+        }
+      }
+
+      // if the endpoint function was found
+      if (currentAction && typeof currentAction === 'function') {
+
+        // if the optional allowedMethods property was defined on endpoint, check that it's honoured
+        if (!!currentAction.allowedMethods) {
+
+          var allowedMethods;
+
+          // allowedMethods can be a list of different methods
+          if (currentAction.allowedMethods instanceof Array) {
+            allowedMethods = currentAction.allowedMethods.map(
+              function (method) {
+                return method.toUpperCase();
+              }
+            );
+          }
+          // allowedMethods can be a single method string
+          if (typeof currentAction.allowedMethods === 'string') {
+            allowedMethods = [currentAction.allowedMethods.toUpperCase()];
+          }
+
+          // stop now and send an error response if the method isn't allowed
+          if (allowedMethods.indexOf(req.method.toUpperCase()) < 0) {
+            return res.status(405).end('METHOD NOT ALLOWED');
+          }
+        }
+
+        // allow api params to be supplied either by json body, or by url query
+        var params = Object.assign({}, req.query);
+        params = Object.assign(params, req.params);
+
+        // either responds to the api call with the resolved promise value
+        // or responds with an error if the promise rejects
+        function handleOutput(promise) {
+          return promise.then(function (result) {
+            return res.json(result);
+          }).catch(function (reason) {
+            if (reason && reason.redirect) {
+              return res.redirect(reason.redirect);
+            } else {
+              console.error('API ERROR:', pe.render(reason));
+              var errorResponse = reason.message;
+              return res.status(reason.status || 500).json(errorResponse);
+            }
+          });
+        }
+
+        // in case the callback is used instead of returning a promise
+        function doneCb(err, result) {
+          if (!!err) {
+            handleOutput(Promise.reject(err));
+          } else {
+            handleOutput(Promise.resolve(result));
+          }
+        }
+
+        // call the api point with the params and request specified
+
+
+        var output = currentAction.apply(null, options.transformParams(req, doneCb));
+
+
+        // if the output isn't an object, then it can't be returned (it is a JSON API), so hopefully the callback will be called
+        if (typeof output === 'object') {
+
+
+          // we make sure the output is promisified before handling it
+          handleOutput(Promise.resolve(output));
+        }
+
+      } else {
+        return res.status(404).end('NOT FOUND');
+      }
+    }
+  } else {
+    throw new Error('expressive-api needs either a string path to the directory, or an object, containing the promise endpoints')
+  }
+};
